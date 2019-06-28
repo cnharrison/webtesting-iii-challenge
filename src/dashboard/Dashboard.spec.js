17da19261d050cfb20d81e79033711d5a0928f11
Christopher Harrison <cnharrison@gmail.com>
2019-06-28 18:25:02 -0400

    dashboard and display tests

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -7,8 +7,16 @@ import Dashboard from "./Dashboard";
 
 describe("<Dashboard />", () => {
   it("matches snapshot while closed and locked by default", () => {
-    const tree = renderer.create(<Dashboard closed={false} locked={false} />);
+    const tree = renderer.create(<Dashboard />);
 
     expect(tree.toJSON()).toMatchSnapshot();
+  });
+  it("displays unlocked and open by default", () => {
+    const message1 = "Unlocked";
+    const message2 = "Open";
+
+    const { getByText } = render(<Dashboard />);
+    getByText(message1);
+    getByText(message2);
   });
 });
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,43 @@
-// Test away!
+import React from "react";
+import renderer from "react-test-renderer";
+import { render, fireEvent } from "@testing-library/react";
+import "@testing-library/react/cleanup-after-each";
+import "jest-dom/extend-expect";
+
+import Display from "./Display";
+
+it("displays closed if gate is closed", () => {
+  const { getByText } = render(<Display closed={true} />);
+
+  getByText("Closed");
+});
+it("displays open if gate is open", () => {
+  const { getByText } = render(<Display closed={false} />);
+
+  getByText("Open");
+});
+it("displays locked if locked", () => {
+  const { getByText } = render(<Display locked={true} />);
+
+  getByText("Locked");
+});
+it("displays unlocked if locked", () => {
+  const { getByText } = render(<Display locked={false} />);
+
+  getByText("Unlocked");
+});
+it("when locked or closed use the red-led class", () => {
+  const { getByText } = render(<Display locked={true} closed={true} />);
+  const indicator1 = getByText("Locked");
+  const indicator2 = getByText("Closed");
+  expect(indicator1).toHaveClass("red-led");
+  expect(indicator2).toHaveClass("red-led");
+});
+
+it("when unlocked or open use the green-led class", () => {
+  const { getByText } = render(<Display locked={false} closed={false} />);
+  const indicator1 = getByText("Unlocked");
+  const indicator2 = getByText("Open");
+  expect(indicator1).toHaveClass("green-led");
+  expect(indicator2).toHaveClass("green-led");
+});
