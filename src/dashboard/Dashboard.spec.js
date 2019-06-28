6673407dd28d39a033d4ed40327caa8b457f2625
Christopher Harrison <cnharrison@gmail.com>
2019-06-28 16:53:53 -0400

    matches snapshot while closed and locked by default

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,14 @@
-// Test away
+import React from "react";
+import renderer from "react-test-renderer";
+import { render, fireEvent } from "@testing-library/react";
+import "@testing-library/react/cleanup-after-each";
+
+import Dashboard from "./Dashboard";
+
+describe("<Dashboard />", () => {
+  it("matches snapshot while closed and locked by default", () => {
+    const tree = renderer.create(<Dashboard closed={false} locked={false} />);
+
+    expect(tree.toJSON()).toMatchSnapshot();
+  });
+});
